4b598155ed59b54395ded4f8c96221c69e6c65fe
Erandi Carolina Vázquez Cuervo <hgftdrd@hotmail.com>
2021-07-17 12:57:17 -0500

    Add the movies table component
    Install dependencies
    Create a connection with the API
    Add table and show the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import TableMovies from "./componets/tableMovies";
 
+export default function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <TableMovies />
+      </Container>
+    </React.Fragment>
+  );
+}
